c15a3b807a22e1084119b1c843ed8d346a70b61c
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-04-23 19:58:41 +0800

    feat: implement <ProductInfo /> PC ui

---
+++ src/components/ProductInfo/ProductInfo.style.ts
@@ -0,0 +1,109 @@
+import styled from "styled-components";
+
+export const CompanyName = styled.h4`
+  color: ${({ theme }) => theme.colors.primary};
+  font-weight: 700;
+  font-size: 14px;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  margin-bottom: 24px;
+`;
+
+export const ProductName = styled.h3`
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 1.2;
+  margin-bottom: 36px;
+`;
+
+export const ProductDescription = styled.p`
+  line-height: 1.5;
+  letter-spacing: 0.8px;
+  color: ${({ theme }) => theme.colors.normalText};
+`;
+
+export const ProductPriceContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  margin-top: 24px;
+`;
+
+export const PriceInfoWrapper = styled.div`
+  display: flex;
+  gap: 16px;
+  align-items: center;
+`;
+
+export const NewPrice = styled.span`
+  font-size: 24px;
+  font-weight: 800;
+`;
+
+export const DiscountWrapper = styled.div`
+  padding: 6px 10px;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  border-radius: 5px;
+
+  span {
+    color: ${({ theme }) => theme.colors.primary};
+    font-weight: 800;
+  }
+`;
+
+export const OriginalPrice = styled.span`
+  color: ${({ theme }) => theme.colors.lightenText};
+  text-decoration: line-through;
+  font-weight: 800;
+`;
+
+export const ActionContainer = styled.div`
+  display: flex;
+  gap: 14px;
+  align-items: center;
+  margin-top: 30px;
+
+  > div {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    &:nth-child(1) {
+      background-color: ${({ theme }) => theme.colors.controllerBackground};
+      border-radius: 8px;
+      padding: 8px;
+    }
+  }
+
+  > div:nth-child(2) {
+    flex: 2;
+  }
+`;
+
+export const ActionController = styled.span<{ disabled?: boolean }>`
+  color: ${({ theme, disabled }) =>
+    disabled ? theme.colors.lightenText : theme.colors.primary};
+  font-weight: 800;
+  font-size: 20px;
+`;
+
+export const NumberText = styled.span`
+  color: ${({ theme }) => theme.colors.black};
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+export const ButtonContent = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  color: ${({ theme }) => theme.colors.white};
+
+  svg {
+    path {
+      fill: ${({ theme }) => theme.colors.white};
+    }
+  }
+`;
---
+++ src/components/ProductInfo/ProductInfo.tsx
@@ -0,0 +1,77 @@
+// hooks
+import useProductQty from "../../hooks/useProductQty";
+
+// components
+import Button from "../../components/UI/Button";
+
+// assets
+import { ReactComponent as CartIcon } from "../../assets/icons/icon-cart.svg";
+
+// styles
+import {
+  CompanyName,
+  ProductName,
+  ProductDescription,
+  ProductPriceContainer,
+  PriceInfoWrapper,
+  NewPrice,
+  DiscountWrapper,
+  OriginalPrice,
+  ActionContainer,
+  ActionController,
+  NumberText,
+  ButtonContent,
+} from "./ProductInfo.style";
+
+const ProductInfo = () => {
+  const { qty, handleChangeQty } = useProductQty();
+
+  console.log(handleChangeQty);
+
+  return (
+    <>
+      <CompanyName>Sneaker Company</CompanyName>
+
+      <ProductName>Fall Limited Edition Sneakers</ProductName>
+
+      <ProductDescription>
+        These low-profile sneakers are your perfect casual wear companion.
+        Featuring a durable rubber outer sole, they’ll withstand everything the
+        weather can offer.
+      </ProductDescription>
+      <ProductPriceContainer>
+        <PriceInfoWrapper>
+          <NewPrice>$125.00</NewPrice>
+          <DiscountWrapper>
+            <span>50%</span>
+          </DiscountWrapper>
+        </PriceInfoWrapper>
+        <OriginalPrice>$250.00</OriginalPrice>
+      </ProductPriceContainer>
+      <ActionContainer>
+        <div>
+          <Button
+            clickFunc={() => handleChangeQty("decrease")}
+            disabled={qty - 1 <= 0}
+          >
+            <ActionController disabled={qty - 1 <= 0}>-</ActionController>
+          </Button>
+          <NumberText>{qty}</NumberText>
+          <Button clickFunc={() => handleChangeQty("increase")}>
+            <ActionController>+</ActionController>
+          </Button>
+        </div>
+        <div>
+          <Button fillType="primary">
+            <ButtonContent>
+              <CartIcon />
+              Add to cart
+            </ButtonContent>
+          </Button>
+        </div>
+      </ActionContainer>
+    </>
+  );
+};
+
+export default ProductInfo;
