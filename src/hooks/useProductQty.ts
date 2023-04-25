49fc7003b4b4c73ee32a9043a0961470f763dfd6
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-04-25 22:24:52 +0800

    feat: implement useProduct hook
    - control product qty

---
+++ src/hooks/useProductQty.ts
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const useProductQty = () => {
+  const [qty, setQty] = useState(1);
+  const handleChangeQty = (type: "increase" | "decrease") => {
+    switch (type) {
+      case "increase":
+        setQty((prevQty) => prevQty + 1);
+        break;
+
+      case "decrease":
+        setQty((prevQty) => prevQty - 1);
+        break;
+    }
+  };
+
+  return {
+    qty,
+    handleChangeQty,
+  };
+};
+
+export default useProductQty;
